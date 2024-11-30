13270c06483b06beddeda6cef224e283929d2fd6
Gokul D <dgokulbecs@gmail.com>
2024-11-30 16:06:29 +0530

    changed file name acc -git

--- src/main/java/com/example/projectrbac/Role_Based_Access_Control.java
+++ src/main/java/com/example/projectrbac/Role_Based_Access_Control.java
@@ -2,15 +2,13 @@ package com.example.projectrbac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 
-public class ProjectrbacApplication {
+public class Role_Based_Access_Control {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectrbacApplication.class, args);
+		SpringApplication.run(Role_Based_Access_Control.class, args);
 		System.out.println("Server is Started!");
 
 	}
